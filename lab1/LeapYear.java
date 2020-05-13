81bb24049e1ba3edf11017cc919a060f022f3ee6
Toni <tonibronars@berkeley.edu>
2020-05-13 09:59:19 -0500

    Lab 1 complete

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -3,6 +3,20 @@
  */
 public class LeapYear {
 
+    public static boolean isLeapYear(int year) {
+        /** Checks whether a year as a command line input is a leap year */
+        if (year % 400 == 0)
+            return true;
+        else if (year % 4 == 0) {
+            if (year % 100 != 0)
+                return true;
+            else
+                return false;
+        }
+        else
+            return false;
+    }
+
     /** Calls isLeapYear to print correct statement.
      *  @param  year to be analyzed
      */
